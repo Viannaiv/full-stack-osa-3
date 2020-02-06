7b95aa1335a2e32cebbd2cc668eee764f5710693
Vivianna Tilander <viva95@windowslive.com>
2020-02-06 16:04:30 +0200

    Tehtävä 3.8*: puhelinluettelon backend step8

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
 
+morgan.token('req-body', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    } 
+
+    return null
+})
+
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 let persons = [
     {
