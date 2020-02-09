8c9ac27f5f1a0d77ce1817f99abcb1d88dca5f85
Vivianna Tilander <viva95@windowslive.com>
2020-02-09 23:29:44 +0200

    Tehtävä 3.18*: puhelinluettelo ja tietokanta, step6

--- index.js
+++ index.js
@@ -19,48 +19,30 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-]
-
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons.map(person => person.toJSON()))
     })
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${Date()}</p>`)
+app.get('/info', (req, res, next) => {
+    Person.estimatedDocumentCount()
+        .then(count => {
+            res.send(`<p>Phonebook has info for ${count} people.</p> <p>${Date()}</p>`)
+        })
+        .catch(error => next(error)) 
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person.toJSON())
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
